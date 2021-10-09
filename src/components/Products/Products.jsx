cac08c9ca025fb079e202af78c6c4f7dbf736930
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-09 19:37:52 -0400

    add products and array products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+
+const products = [
+    {
+        id: 1,
+        name: 'T-Shirt-Female-1',
+        description: 'Simba and Nala T-Shirt Female'
+    },
+    {
+        id: 2,
+        name: 'T-Shirt-Male-1',
+        description: 'Simba and Nala T-Shirt Male'
+    },
+]
+
+const Products = () => {
+    <main>
+      <Grid container justify="center" spacing={4}>
+          
+      </Grid>
+    </main>
+}
+
+export default Products;
