fa883ebf55fd9bf604e1fc56db1ef993002d6d5e
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-11 20:45:10 -0400

    finish navbar styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-//  import Products from './components/Products/Products';
+// import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
 
 import { Products, Navbar } from './components';
