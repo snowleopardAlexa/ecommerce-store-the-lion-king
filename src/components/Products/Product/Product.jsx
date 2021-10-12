70d6371961f702b19a8858ee1e3bd36de0fa972a
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-12 18:47:29 -0400

    call product from the store

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -9,18 +9,17 @@ const Product = ({ product }) => {
 
     const classes = useStyles();
 
-    console.log(product);
-
-    return <div>test</div>
-
     return (
         <div>
             <Card className={classes.root}>
-              <CardMedia className={classes.media} image={product.image} title={product.name} />
+              <CardMedia className={classes.media} image={product.media.source} title={product.name} />
               <CardContent>
                   <div className={classes.CardContent}>
+                  <Typography variant="h5" gutterBottom>
+                          {product.name}
+                      </Typography>
                       <Typography variant="h5" gutterBottom>
-                          {product.price}
+                          {product.price.formatted_with_symbol}
                       </Typography>
                   </div>
                   <Typography variant="body2" color="textSecondary">{product.description}</Typography>
