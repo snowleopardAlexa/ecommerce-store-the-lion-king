7d7e70d8add087a24905409dbb3546dcd357cb10
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-12 21:47:06 -0400

    add promise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
-// import Products from './components/Products/Products';
-// import Navbar from './components/Navbar/Navbar';
+import React, { useState, useEffect} from 'react';
+// import store
+import { commerce } from './lib/commerce';
 
 import { Products, Navbar } from './components';
 
 const App = () => {
+
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
     return (
         <div>
             <Navbar />
