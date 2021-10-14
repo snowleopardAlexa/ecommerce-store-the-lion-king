be1fc63ed4c036ec2deba60c7f999e0ed81116cf
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-14 18:22:34 -0400

    add final layout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/icons';
-import { classes } from 'istanbul-lib-coverage';
+
+
 
 const Cart = ({ cart }) => {
 
     const isEmpty = !cart.line_items.length;
+  
 
     const EmptyCart = () => {
         <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
@@ -13,8 +15,21 @@ const Cart = ({ cart }) => {
     const filledCart = () => {
         <>
           <Grid container spacing={3}>
-
+             {cart.line_items.EmptyCart((item) => (
+                <Grid item xs={12} sm={4} key={item.id}>
+                   <div>{item.name}</div>
+                </Grid>
+             ))}
           </Grid>
+          <div className={classes.cardDetails}>
+              <Typography variant="h4">
+                  Subtotal: { cart.subtotal.formatted_with_symbol}
+              </Typography>
+              <div>
+                  <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+                  <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout Cart</Button>
+              </div>
+          </div>
         </>
     }
 
