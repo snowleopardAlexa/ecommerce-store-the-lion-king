49761d43d4f8fba17ee2b86608e2b20590e89318
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-14 18:11:57 -0400

    add cart layout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,11 +3,27 @@ import { Container, Typography, Button, Grid } from '@material-ui/icons';
 import { classes } from 'istanbul-lib-coverage';
 
 const Cart = () => {
+
+    const isEmpty = true;
+
+    const EmptyCart = () => {
+        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+    }
+
+    const filledCart = () => {
+        <>
+          <Grid container spacing={3}>
+              
+          </Grid>
+        </>
+    }
+
     return (
         <div>
             <Container>
                 <div className={classes.toolbar} />
                 <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+                { isEmpty ? <EmptyCart /> : <FilledCart /> }
             </Container>
         </div>
     )
