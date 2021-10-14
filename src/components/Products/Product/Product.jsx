27b4bc9bad6b08345fb30398da305b3e3fd0fd89
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-13 22:41:10 -0400

    change the name of the api to call cards and the size of the media img

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -9,20 +9,21 @@ const Product = ({ product }) => {
 
     const classes = useStyles();
 
+
     return (
         <div>
             <Card className={classes.root}>
-              <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+              <CardMedia className={classes.media} image={product.image.url} title={product.name} />
               <CardContent>
-                  <div className={classes.CardContent}>
-                  <Typography variant="h5" gutterBottom>
+                  <div className={classes.cardContent}>
+                      <Typography variant="h5" gutterBottom>
                           {product.name}
                       </Typography>
                       <Typography variant="h5" gutterBottom>
                           {product.price.formatted_with_symbol}
                       </Typography>
                   </div>
-                  <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+                  <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" />
               </CardContent>
               <CardActions disableSpacing className={classes.CardActions}>
                   <IconButton aria-label="Add to Cart">
