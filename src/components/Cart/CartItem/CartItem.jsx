57d7275812904cf0928f93afe66beb5580ae9a20
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-15 19:18:55 -0400

    add cart item component

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const CartItem = () => {
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CartItem;
