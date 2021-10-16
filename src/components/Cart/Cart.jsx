69ff4eabe070577dde4781ff4aa0d538f9499e38
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-16 18:14:46 -0400

    fix typo item

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = ({ cart }) => {
           <Grid container spacing={3}>
              {cart.line_items.map((item) => (
                 <Grid item xs={12} sm={4} key={item.id}>
-                   <CartItem itm={item} />
+                   <CartItem item={item} />
                 </Grid>
              ))}
           </Grid>
@@ -39,7 +39,7 @@ const Cart = ({ cart }) => {
         <div>
             <Container>
                 <div className={classes.toolbar} />
-                <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
+                <Typography className={classes.title} variant="h4" gutterBottom>Your Shopping Cart</Typography>
                 { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
             </Container>
         </div>
