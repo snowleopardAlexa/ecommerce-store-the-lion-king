f2ecbea7e0d5501d19e70fc41a52a1f78ddbc5a6
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-17 19:10:07 -0400

    import forminput from customtext field to addressform

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
+import FormInput from './CustomTextField';
+
 const AddressForm = () => {
     return (
         <>
          <Typography variant="h6">
              <FormProvider {... methods}>
-                 <form onSubmit={}>
+                 <form onSubmit=''>
                      <Grid container spacing={3}>
-                         
+                       <FormInput required name='firstName' label='First Name' />
                      </Grid>
                  </form>
              </FormProvider>
