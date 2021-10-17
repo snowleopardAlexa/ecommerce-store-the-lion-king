685c1340d9ccb562cd4235d827c5ce11f8ad240a
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-17 18:37:42 -0400

    add styles

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import { classExpression } from '@babel/types';
+
+import useStyles from './styles';
 
 const steps = ['Shipping address', 'Payment details'];
 
 export const Checkout = () => {
+
+     const [activeStep, setActiveStep] = useState(0);
+     const classes = useStyles();
+
     return (
         <>
         <main>
-          <div className={classExpression.toolbar} />
-          <main className={classExpression.layout} />
-            <Paper className={classExpression.paper}>
+          <div className={classes.toolbar} />
+          <main className={classes.layout} />
+            <Paper className={classes.paper}>
                 <Typography variant="h4" align="center">Checkout</Typography>
-                <Stepper activeStep={0} className={classExpression.stepper}>
+                <Stepper activeStep={0} className={classes.stepper}>
                      {steps.map((step) => (
                          <Step key={step}>
                              <StepLabel>{step}</StepLabel>
