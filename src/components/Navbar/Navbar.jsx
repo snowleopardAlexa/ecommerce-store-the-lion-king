21d3a2b17924969861ba1931693d30b0bd41b08d
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-16 20:15:02 -0400

    hide the cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/lion-facing-right.png'
 import useStyles from './styles';
@@ -9,23 +9,28 @@ import useStyles from './styles';
 const Navbar = ({ totalItems }) => {
 
     const classes = useStyles();
+    const location = useLocation();
+
+    if (location.pathname === '/')
 
     return (
         <>
           <AppBar position="fixed" className={classes.AppBar} color="inherit">
               <Toolbar>
-                  <Typography variant="h6" className={classes.AppBar} color="inherit">
+                  <Typography component={Link} to="/" variant="h6" className={classes.AppBar} color="inherit">
                       <img src={logo} alt="The Lion King Store" height="25px" className={classes.image} />
                       The Lion King Store
                   </Typography>
                   <div className={classes.grow} />
-                  <div className={classes.button}>
+                  {location.pathname === '/' && (
+                      <div className={classes.button}>
                       <IconButton component={Link} to="/cart" aria-label="show cart items" color="inherit">
                           <Badge badgeContent={totalItems} color="secondary">
                               <ShoppingCart />
                           </Badge>
                       </IconButton>
                   </div>
+                  )}
               </Toolbar>
           </AppBar>
         </>
