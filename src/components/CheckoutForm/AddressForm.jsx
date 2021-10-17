886080d1537dcb0e329ffe7df557c077e14f8e48
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-17 19:03:04 -0400

    add addressform layout

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
 
 const AddressForm = () => {
     return (
-        <div>
-            addressform
-        </div>
+        <>
+         <Typography variant="h6">
+             <FormProvider {... methods}>
+                 <form onSubmit={}>
+                     <Grid container spacing={3}>
+                         
+                     </Grid>
+                 </form>
+             </FormProvider>
+         </Typography>
+        </>
     )
 }
 
