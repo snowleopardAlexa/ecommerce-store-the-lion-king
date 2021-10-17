5e00219deda3abb2eaff3cd0c1a42ce2442345d1
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-17 18:03:28 -0400

    add onUpdateCartQty to buttons plus and minus to update the cart quantity

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -3,7 +3,7 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 
 import useStyles from './styles';
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
 
     const classes = useStyles();
 
@@ -16,9 +16,9 @@ const CartItem = ({ item }) => {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <div className={classes.buttons}>
-                    <Button type="button" size="small">-</Button>
+                    <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type="button" size="small">+</Button>
+                    <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
                 </div>
                 <Button variant="contained" type="button" color="secondary">Remove</Button>
             </CardActions>
