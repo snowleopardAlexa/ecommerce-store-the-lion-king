109c7527e9370e2996beb0a2ad60cf2532b0aef2
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-17 19:25:27 -0400

    add render to controller, fix

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -9,15 +9,18 @@ const FormInput = ({ name, label, required }) => {
     return (
        <Grid item xs={12} sm={6}>
            <Controller 
-             as={TextField}
-             control={control}
-             fullWidth
-             name={name}
-             label={label}
-             required={required}
+             render={({ field }) => (
+                <TextField 
+                {...field}
+                label={label}
+                required={required} 
+                />)}
+                control={control}
+                fullWidth
+                name={name}
            />
        </Grid>
-    )
+    );
 }
 
 export default FormInput;
