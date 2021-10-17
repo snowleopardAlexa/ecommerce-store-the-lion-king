3ceb3a4414d150406f88c67725043070dc1a582a
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-16 20:05:05 -0400

    add link router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/lion-facing-right.png'
 import useStyles from './styles';
@@ -19,6 +20,7 @@ const Navbar = ({ totalItems }) => {
                   </Typography>
                   <div className={classes.grow} />
                   <div className={classes.button}>
+                      <Link to="/cart">go to cart</Link>
                       <IconButton aria-label="show cart items" color="inherit">
                           <Badge badgeContent={totalItems} color="secondary">
                               <ShoppingCart />
