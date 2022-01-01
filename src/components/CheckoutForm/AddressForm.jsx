f85500b68434ba0b0e83b901affb0abe8ab0a721
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-01 18:49:18 -0500

    add btn

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 import { commerce } from '../../lib/commerce';
 
@@ -99,6 +100,11 @@ const AddressForm = ( { checkoutToken }) => {
                         </Select>
                        </Grid> 
                     </Grid>
+                    <br />
+                    <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                        <Button component={Link} to="/cart" variant="outlined">Back to Cart</Button>
+                        <Button type="submit" variant="contained" color="primary">Next</Button>
+                    </div>
                  </form>
              </FormProvider>
         </>
