c4b3f0baa0c75db3e816a53eff07f02baefa5021
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-03 21:31:29 -0500

    add order firstname and lastname

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -47,9 +47,9 @@ export const Checkout = ( { cart, order, onCaptureCheckout, error }) => {
      let Confirmation = () => order.customer ? (
          <>
            <div>
-               <Typography variant="h5">Thank you for your purchase, firstName, lastName</Typography>
+               <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname} </Typography>
                <Divider className="{classes.divider}" />
-               <Typography variant="subtitle2">Order ref: ref</Typography>
+               <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
            </div> 
            <br />
            <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
